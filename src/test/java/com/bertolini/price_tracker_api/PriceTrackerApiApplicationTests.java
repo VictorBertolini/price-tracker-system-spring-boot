d7902c40d487291a8a9224fea1dad4da00c36cbc
Bertolini <victorbertolinidesousa@gmail.com>
2026-01-14 01:54:32 -0300

    add: Adicionando arquivos externos do maven e pasta de testes

---
+++ src/test/java/com/bertolini/price_tracker_api/PriceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bertolini.price_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
