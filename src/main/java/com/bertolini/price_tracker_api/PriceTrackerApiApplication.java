489af3d9d12ff73f27c56d3f13a473ed2d80aa93
Bertolini <victorbertolinidesousa@gmail.com>
2026-03-03 00:08:23 -0300

    add: Permitindo agendamento de funcionalidade

--- src/main/java/com/bertolini/price_tracker_api/PriceTrackerApiApplication.java
+++ src/main/java/com/bertolini/price_tracker_api/PriceTrackerApiApplication.java
@@ -2,8 +2,10 @@ package com.bertolini.price_tracker_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PriceTrackerApiApplication {
 
 	public static void main(String[] args) {
